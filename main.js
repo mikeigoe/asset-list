5abd0668b6aa15680b8ad543445e668cc4361ac4
Mike Igoe <mikeigoe@gmail.com>
2017-12-26 16:46:06 +0000

    daily update

--- main.js
+++ main.js
@@ -4,20 +4,49 @@ const url = require('url');
 const path = require('path');
 
 // Pull some required features out of electron
-const {app, BrowserWindow} = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 // Declare our main window
 let mainWindow;
 
 // Listen for the app to be ready
-app.on('ready', function(){
-    // Create new window
-    mainWindow = new BrowserWindow({});
-    // Load HTML into window
-    mainWindow.loadURL(url.format({
-        // This is basically passing file://dirname/mainWindow.html into loadUrl
-        pathname: path.join(__dirname, 'mainWindow.html'), //__dirname is the current directory
-        protocol: 'file:',
-        slashes: true
-    }));
+app.on('ready', function () {
+	// Create new window
+	mainWindow = new BrowserWindow({});
+	// Load HTML into window
+	mainWindow.loadURL(url.format({
+		// This is basically passing file://dirname/mainWindow.html into loadUrl
+		pathname: path.join(__dirname, 'mainWindow.html'), //__dirname is the current directory
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	// Build menu from template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+	Menu.setApplicationMenu(mainMenu);
 });
+
+// Create main menu template
+
+const mainMenuTemplate = [
+	{
+		label: 'File',
+		submenu:[
+			{
+				label: 'Add Item'
+			},
+			{
+				label: 'Clear Items'
+			},
+			{
+				label: 'Quit',
+				// Hotkey for quit, 'darwin' means we are running on a Mac, else Windows or Linux
+				accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+				click(){
+					app.quit();
+				}
+			}
+		]
+	}
+];
